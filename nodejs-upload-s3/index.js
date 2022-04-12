1d5ed7de109a98fded764409d07efd4dacbb493c
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-12 19:50:27 -0300

    refactor: feature to upload from Express/Multer to AWS + pass to ES6

--- nodejs-upload-s3/index.js
+++ nodejs-upload-s3/index.js
@@ -3,7 +3,9 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import multer from "multer"
+import path from 'path'
 import uploadToS3 from './uploadToS3.js'
+// import uploadFile from './uploadFile.js'
 
 const upload = multer({ dest: "uploads/" })
 
@@ -13,8 +15,14 @@ app.use(cors());
 const PORT = process.env.PORT || 4200;
 app.post('/upload', upload.any(), async (req, res) => {
   try {
+    const uploadedImage = await uploadToS3({
+      fileBinary: req.files[0],
+      fileName: req.files[0].originalname,
+      fileExtension: path.extname(req.files[0].originalname),
+      filePath: req.files[0].path,
+    });
 
-    const uploadedImage = await uploadToS3(req.files, 'uploads', new Date().getMilliseconds());
+
     res.status(200).json(uploadedImage)
   } catch (error) {
     res.status(400).json(error.message)
